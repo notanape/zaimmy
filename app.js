81926b0ca4cf1c2e8f09963d47a640f1ce9ae371
Maksym <maxim.starchukov@gmail.com>
2018-12-22 15:35:21 +0200

    Propper index

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+//app.use(express.static(path.join(__dirname, 'public')));
 app.set('public', path.join(__dirname, 'public'));
-app.set('port', '8001');
+app.set('port', '8765');
+
+app.get("/*", (req, res) => {
+    let param = req.params[0];
+    fs.stat(app.get("public") + "/" + param, (err, st) => {
+        if (err) {
+            res.status(404).end()
+        }
+        if (param.includes('.well-known')){
+            res.sendFile(app.get("public") + "/" + param, {
+                headers: {
+                    'Content-Type': 'text/plain'
+                }
+            })}
+        else
+            res.sendFile(app.get("public") + "/" + param)
+    })
+})
 
 app.listen(app.get('port'), () => {
     console.log(`Port ${app.get('port')} is running...`)
 });
+
+///.well-known/acme-challenge/06I9v1vWDVTRNdFFad8o3Fke92gfl37fQjUkCvYzXqs
