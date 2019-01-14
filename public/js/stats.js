f03f7c8947d02aab0f38ed03732acd6b8428c95e
Maksym <maxim.starchukov@gmail.com>
2019-01-14 18:38:00 +0200

    stats

---
+++ public/js/stats.js
@@ -0,0 +1,339 @@
+let _stat = [{
+    "_id": "5c3b8353e6a943118082fe23",
+    "ip": "66.249.89.60",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-13",
+    "time": "20:28"
+}, {
+    "_id": "5c3b8387e6a943118082fe24",
+    "ip": "91.197.51.31",
+    "ref": "https://www.google.com/",
+    "shop": "index",
+    "day": "2019-1-13",
+    "time": "20:29"
+}, {
+    "_id": "5c3b83a6e6a943118082fe25",
+    "ip": "66.102.9.62",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-13",
+    "time": "20:29"
+}, {
+    "_id": "5c3b90d4e6a943118082fe26",
+    "ip": "52.53.201.78",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-13",
+    "time": "21:26"
+}, {
+    "_id": "5c3b97edbd72040318c7ec09",
+    "ip": "178.73.215.171",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-13",
+    "time": "21:56"
+}, {
+    "_id": "5c3b9917bd72040318c7ec0a",
+    "ip": "177.185.159.146",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-13",
+    "time": "22:01"
+}, {
+    "_id": "5c3ba4d428694610445929fe",
+    "ip": "46.248.51.165",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-13",
+    "time": "22:51"
+}, {
+    "_id": "5c3ba71528694610445929ff",
+    "ip": "189.38.158.130",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-13",
+    "time": "23:01"
+}, {
+    "_id": "5c3baf082869461044592a00",
+    "ip": "79.103.36.22",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-13",
+    "time": "23:35"
+}, {
+    "_id": "5c3bbd102869461044592a01",
+    "ip": "46.151.244.51",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "00:34"
+}, {
+    "_id": "5c3bc3f52869461044592a02",
+    "ip": "202.57.52.158",
+    "ref": "http://zaimmy.online/",
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "01:04"
+}, {
+    "_id": "5c3bf2342869461044592a03",
+    "ip": "112.8.49.219",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "04:21"
+}, {
+    "_id": "5c3bfb462869461044592a04",
+    "ip": "84.10.60.22",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "05:00"
+}, {
+    "_id": "5c3bff552869461044592a05",
+    "ip": "177.107.20.206",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "05:17"
+}, {
+    "_id": "5c3c07f92869461044592a06",
+    "ip": "103.78.218.51",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "05:54"
+}, {
+    "_id": "5c3c08f12869461044592a07",
+    "ip": "178.128.194.144",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "05:58"
+}, {
+    "_id": "5c3c0a732869461044592a08",
+    "ip": "131.221.192.157",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "06:05"
+}, {
+    "_id": "5c3c178f2869461044592a09",
+    "ip": "141.105.64.201",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "07:01"
+}, {
+    "_id": "5c3c18a4a62aa108fc01c3df",
+    "ip": "34.221.118.176",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "07:05"
+}, {
+    "_id": "5c3c4d9d6985060a64b10717",
+    "ip": "47.104.174.28",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "10:51"
+}, {
+    "_id": "5c3c4dab9cc97b11b0e3e056",
+    "ip": "47.104.174.28",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "10:51"
+}, {
+    "_id": "5c3c69e0831b7e0d3ccef273",
+    "ip": "159.65.24.22",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "12:52"
+}, {
+    "_id": "5c3c7a6ea03d9e0cf0ca80db",
+    "ip": "200.54.111.18",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "14:02"
+}, {
+    "_id": "5c3c7c51a03d9e0cf0ca80dc",
+    "ip": "91.245.112.241",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "14:10"
+}, {
+    "_id": "5c3c7f7ca03d9e0cf0ca80dd",
+    "ip": "84.241.43.38",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "14:24"
+}, {
+    "_id": "5c3c7fb1a03d9e0cf0ca80de",
+    "ip": "52.53.201.78",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "14:25"
+}, {
+    "_id": "5c3c815ea03d9e0cf0ca80df",
+    "ip": "37.235.237.0",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "14:32"
+}, {
+    "_id": "5c3c83bda03d9e0cf0ca80e0",
+    "ip": "45.71.228.20",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "14:42"
+}, {
+    "_id": "5c3c84aa03e39109d047067b",
+    "ip": "194.183.183.142",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "14:46"
+}, {
+    "_id": "5c3c86745c3b5510449e3434",
+    "ip": "66.102.9.2",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "14:54"
+}, {
+    "_id": "5c3c8761ff15bc06e850e9ec",
+    "ip": "36.66.108.167",
+    "ref": null,
+    "shop": "index",
+    "day": "2019-1-14",
+    "time": "14:58"
+}];
+
+_ip = {};
+for (let i of _stat) {
+    let arr = i.day.split('-');
+    let mon = arr[1] + '.' + arr[0];
+    let day = arr[2];
+    if (!_ip.hasOwnProperty(i.ip)) {
+        _ip[i.ip] = {};
+        _ip[i.ip][mon] = {
+            [day]: {
+                [i.time]: [
+                    [i.ref, i.shop]
+                ]
+            }
+        }
+    } else {
+        if (!_ip[i.ip].hasOwnProperty(mon)) {
+            _ip[i.ip][mon] = {
+                [day]: {
+                    [i.time]: [
+                        [i.ref, i.shop]
+                    ]
+                }
+            }
+        } else {
+            if (!_ip[i.ip][mon].hasOwnProperty(day)) {
+                _ip[i.ip][mon][day] = {
+                    [i.time]: [
+                        [i.ref, i.shop]
+                    ]
+                }
+            } else {
+                _ip[i.ip][mon][day][i.time].push([i.ref, i.shop])
+            }
+        }
+
+    }
+}
+
+_map = [];
+for (let i in _ip) {
+    let ip = [i];
+    let colM = [];
+    for (let y in _ip[i]) {
+        let mo = [y];
+        let colD = [];
+        for (let z in _ip[i][y]) {
+            let da = [z];
+            let colT = [];
+            for (let t in _ip[i][y][z]) {
+                let ti = [t];
+                let [...f] = _ip[i][y][z][t];
+                ti.push(f);
+                colT.push(ti)
+            }
+            da.push(colT);
+            colD.push(da)
+        }
+        mo.push(colD);
+        colM.push(mo)
+    }
+    ip.push(colM)
+    _map.push(ip)
+}
+
+function sortTime(a, b) {
+    let _a = parseInt(a[0].split(':').join(''))
+    let _b = parseInt(b[0].split(':').join(''))
+    return _a - _b
+}
+
+function sortDay(a, b) {
+    let _a = parseInt(a[0])
+    let _b = parseInt(b[0])
+    return _a - _b
+}
+
+function sortMon(a, b) {
+    let _a = parseInt(a[0].split('.').join(''))
+    let _b = parseInt(b[0].split('.').join(''))
+    return _a - _b
+}
+
+function sortIp(a, b) {
+    let _onesA = true;
+    let _onesB = true;
+    if (a[1].length > 1)
+        _onesA = false
+    else {
+        if (a[1][1].length > 1)
+            _onesA = false
+        else {
+            if (a[1][1][1].length > 1)
+                _onesA = false
+        }
+    }
+    if (b[1].length > 1)
+        _onesB = false
+    else {
+        if (b[1][1].length > 1)
+            _onesB = false
+        else {
+            if (b[1][1][1].length > 1)
+                _onesB = false
+        }
+    }
+    if(_onesA>_onesB)
+    	return 1
+    else if(_onesA<_onesB)
+    	return -1
+    else if(_onesA == _onesB){
+    	let _ipDataA = a[1][1][a[1][1].length-1];
+    	let _monA = _ipDataA[0];
+    	let _marrA = _monA.split('.');
+    	let _monDataA = _ipDataA[1][_ipDataA[1].length-1];
+    	let _dayA = _monDataA[0];
+    	let _dayDataA = _monDataA[1][_monDataA[1].length-1];
+    	let _timeA = _dayDataA[0];
+    	let _dA = new Date(`${_marrA[0]}.${_dayA}.${_marrA[1]} ${_timeA}`).getTime()
+    }
+}
